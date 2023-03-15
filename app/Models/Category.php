76657192af9725d005aa630d603a72efb6d13864
Irfan Azhar <muhammad.irfan.azhar@mail.ugm.ac.id>
2023-03-16 00:47:09 +0700

    creating category factory and seeder

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,4 +8,30 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'is_published',
+    ];
+
+    protected $casts = [
+        'is_published' => 'boolean',
+    ];
+
+    public function scopePublished($query)
+    {
+        return $query->where('is_published', true);
+    }
+
+    public function scopeUnpublished($query)
+    {
+        return $query->where('is_published', false);
+    }
+
+    public function search($q)
+    {
+        return $this->where('name', 'like', "%{$q}%");
+    }
+
+    
 }
